5b397ab9cf29055b6dc006763b932f11c8f04e8d
SStar2 <19593120-SStar2@users.noreply.replit.com>
2023-09-21 03:46:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Welcome to my Elite 101 Chatbox!")
+
+name = input("Please enter your name: ")
+user_name = str(name)
+print("Hello " + name +"!")
+
+
+age = input("How old are you? ")
+user_age = int(age)
+print("So "+ name + ", you're " + str(user_age) + " years old!")
+print("Thank you for the information. How can I help you today?")
+
+print("\nPlease choose from the following options")
+
+print("1. Placeholder Option 1")
+print("2. Placeholder Option 2")
+print("3. Placeholder Option 3")
+print("4. Exit the conversation")
+
+option = input("Enter the number of your choice: ")
+
